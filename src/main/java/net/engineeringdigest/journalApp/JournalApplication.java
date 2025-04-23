cbd408a8a26c97df9d410cf1831409989b56b555
Hemant <hemantpatwa48@gmail.com>
2025-04-24 00:51:43 +0530

    done CRUD operation with using HashMap

--- src/main/java/net/engineeringdigest/journalApp/JournalApplication.java
+++ src/main/java/net/engineeringdigest/journalApp/JournalApplication.java
@@ -3,19 +3,16 @@ package net.engineeringdigest.journalApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
-@RestController
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class JournalApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(JournalApplication.class, args);
     }
 
-    @GetMapping("hem")
-    public String fun(){
-        return "This is Kumar";
-    }
+
 }
